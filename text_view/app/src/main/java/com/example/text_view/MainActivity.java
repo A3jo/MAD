f56945d0c01bfba11ca848f347464da9e9593f5f
Your Name <you@example.com>
2024-09-05 12:20:58 +0530

    initial commit

---
+++ text_view/app/src/main/java/com/example/text_view/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.text_view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText Ed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Ed=(EditText)findViewById(R.id.text1);
+    }
+
+    public void submit(View view) {
+        Ed.setText("Hello world");
+    }
+
+    public void click1(View view) {
+        Toast.makeText(this, Ed.getText(), Toast.LENGTH_SHORT).show();
+    }
+}
